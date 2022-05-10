348e4b5320bbb7dbe24342880f60b200b33c1d3e
onlyphantom <samuel@outlook.sg>
2022-05-10 21:30:05 +0700

    initial commit

---
+++ src/api/tag-award/controllers/tag-award.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  tag-award controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tag-award.tag-award');
---
+++ src/api/tag-award/services/tag-award.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tag-award service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tag-award.tag-award');
