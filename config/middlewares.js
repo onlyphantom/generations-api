dc3223e613c4bef6cdd0f4fadfd478d9d78ecf3a
onlyphantom <samuel@outlook.sg>
2022-06-04 17:56:01 +0700

    add localhost 1337

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,11 @@ module.exports = [
     config: {
       headers: ['*'],
       origin: [
-        'http://localhost:3000', 'https://thegenerations.netlify.app', 'https://fellowship.supertype.ai', 'https://generationsapi.herokuapp.com']
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'https://thegenerations.netlify.app',
+        'https://fellowship.supertype.ai',
+        'https://generationsapi.herokuapp.com']
     }
   },
   'strapi::poweredBy',
